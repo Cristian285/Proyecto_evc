6f12c0fae6d7a2727d062ec593b16f4d3bbd57b4
CCamiloArenas <dltnascrist285@outlook.com>
2024-09-25 22:07:15 -0500

    cambio index php

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventos Cumanday</title>
+</head>
+<body>
+    <header class="container__header">
+
+    </header>
+</body>
+</html>
